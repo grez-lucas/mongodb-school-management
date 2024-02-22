2cfca931363a31f8371a9c8a272577e23fff4a7b
grez-lucas <lucasgrez3010@gmail.com>
2024-02-22 17:04:12 -0300

    [feat] lessonById query

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver((_of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query((_returns) => LessonType)
   lesson() {
     return {
@@ -12,5 +14,20 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Query((_returns) => LessonType)
+  lessonById(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation((_returns) => LessonType)
+  createLesson(
+    @Args('name') name: string, // This allows us to retrieve individual arguments
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    // We will create a service that handles Lesson creation logic!
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Lesson } from './lesson.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) // The lesson entity will be injected into this service
+    private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(
+    name: string,
+    startDate: string,
+    endDate: string,
+  ): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+    });
+
+    return await this.lessonRepository.save(lesson);
+  }
+
+  async getLesson(id: string): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({ where: { id } });
+    return lesson;
+  }
+}
