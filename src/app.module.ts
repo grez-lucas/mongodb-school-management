23691075ef4f79f133a0cb4fe5ff1ad51f9d5634
grez-lucas <lucasgrez3010@gmail.com>
2024-02-22 16:07:11 -0300

    [fix] GraphQL Apollo setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver, // We will use Apollo as the driver
       autoSchemaFile: true, // We will use decorators to save the schema, and regenerate it on each start
     }),
     LessonModule,
